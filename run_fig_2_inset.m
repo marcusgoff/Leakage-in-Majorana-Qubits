0aa481c2d03cba35801532366c71cff56a667c69
marcusgoff <116776757+marcusgoff@users.noreply.github.com>
2025-09-04 15:15:40 +1000

    Update run_fig_2_inset.m

--- run_fig_2_inset.m
+++ run_fig_2_inset.m
@@ -29,7 +29,7 @@
 %   - Saves entire worskpace in the ./results directory
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-disp('Running run_fig_2_main.m');
+disp('Running run_fig_2_inset.m');
 
 %% Environment Setup
 clear all; 
